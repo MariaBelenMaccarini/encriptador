afe481227936a0011020cdd237c924554e4a9932
Maria Belen Maccarini <rosalie.juani.aaron2014@gmail.com>
2022-09-20 15:14:44 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var inputTexto = document.querySelector(".IngreseTexto")
+var mensaje = document.querySelector(".MensajeIngresado")
+
+function btnEncriptar() {
+    var MensajeEncriptado = encriptar(inputTexto.value)
+    mensaje.value = MensajeEncriptado;
+    mensaje.style.backgroundImage = "none"
+    inputTexto.value = ""
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    var MensajeDesencriptado = desencriptar(inputTexto.value)
+    mensaje.value = MensajeDesencriptado;
+    mensaje.style.backgroundImage = "none"
+    inputTexto.value = ""
+}
+
+function desencriptar(stringDesencriptada) {
+     let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+     stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+         if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+                stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+}
+
+function copiar() {
+    mensaje.select()
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = ""
+}
